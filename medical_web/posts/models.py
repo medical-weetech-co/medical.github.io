a819d70663fc519ccc797582937239d048ab9a80
Jason Wee <peichieh@gmail.com>
2016-11-28 19:28:02 +0800

    learn on property decorator and added function comments and get_content_type

--- medical_web/posts/models.py
+++ medical_web/posts/models.py
@@ -1,5 +1,6 @@
 
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
@@ -9,6 +10,7 @@ from django.utils.safestring import mark_safe
 from django.utils.text import slugify
 
 from markdown_deux import markdown
+from comments.models import Comment
 # Create your models here.
 
 
@@ -61,6 +63,18 @@ class Post(models.Model):
 
 	class Meta:
 		ordering = ["-timestamp", "-updated"]
+
+	@property
+	def comments(self):
+		instance = self
+		qs = Comment.objects.filter_by_instance(self)
+		return qs
+
+	@property
+	def get_content_type(self):
+		instance = self
+		content_type = ContentType.objects.get_for_model(instance.__class__)
+		return content_type
 
 def create_slug(instance, new_slug=None):
 	slug = slugify(instance.title)
