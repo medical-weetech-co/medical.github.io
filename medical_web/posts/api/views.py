9c3712fe0beed3943a89f3625ccc4562cea37db8
Jason Wee <peichieh@gmail.com>
2016-12-03 00:24:14 +0800

    change to RetrieveUpdateAPIView, added PostCreateAPIView

--- medical_web/posts/api/views.py
+++ medical_web/posts/api/views.py
@@ -1,14 +1,37 @@
 from rest_framework.generics import (
+	CreateAPIView,
 	DestroyAPIView,
 	ListAPIView, 
 	UpdateAPIView, 
 	RetrieveAPIView,
+	RetrieveUpdateAPIView,
+	)
+
+from rest_framework.permissions import (
+	AllowAny,
+	IsAuthenticated,
+	IsAdminUser,
+	IsAuthenticatedOrReadOnly,
 	)
 
 from posts.models import Post
 
-from .serializers import PostDetailSerializer, PostListSerializer
+from .permissions import IsOwnerOrReadOnly
 
+from .serializers import (
+	PostCreateUpdateSerializer, 
+	PostDetailSerializer, 
+	PostListSerializer
+	)
+
+
+class PostCreateAPIView(CreateAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostCreateUpdateSerializer
+	permission_classes = [IsAuthenticated]
+
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user)
 
 class PostDetailAPIView(RetrieveAPIView):
 	queryset = Post.objects.all()
@@ -16,11 +39,15 @@ class PostDetailAPIView(RetrieveAPIView):
 	lookup_field = 'slug'
 	#lookup_url_kwarg = 'abc'
 
-class PostUpdateAPIView(UpdateAPIView):
+class PostUpdateAPIView(RetrieveUpdateAPIView):
 	queryset = Post.objects.all()
-	serializer_class = PostDetailSerializer
+	serializer_class = PostCreateUpdateSerializer
 	lookup_field = 'slug'
-	#lookup_url_kwarg = 'abc'	
+	#lookup_url_kwarg = 'abc'
+	permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+	def perform_update(self, serializer):
+		serializer.save(user=self.request.user)
 
 class PostDeleteAPIView(DestroyAPIView):
 	queryset = Post.objects.all()
