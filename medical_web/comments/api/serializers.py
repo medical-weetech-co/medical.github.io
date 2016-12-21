5f0113d727cf0fb989258d27a6c40ebd14d73e55
Jason Wee <peichieh@gmail.com>
2016-12-21 13:15:45 +0800

    added user detail serializer so able to display fields

--- medical_web/comments/api/serializers.py
+++ medical_web/comments/api/serializers.py
@@ -8,6 +8,8 @@ from rest_framework.serializers import (
 	ValidationError,
 	)
 
+from accounts.api.serializers import UserDetailSerializer
+
 from comments.models import Comment
 
 user = get_user_model()
@@ -102,15 +104,18 @@ class CommentListSerializer(ModelSerializer):
 		return 0
 
 class CommentChildSerializer(ModelSerializer):
+	user = UserDetailSerializer(read_only=True)
 	class Meta:
 		model = Comment
 		fields = [
 		    'id',
+		    'user',
 		    'content',
 		    'timestamp'
 		]
 
 class CommentDetailSerializer(ModelSerializer):
+	user = UserDetailSerializer(read_only=True)
 	reply_count = SerializerMethodField()
 	content_object_url = SerializerMethodField()
 	replies = SerializerMethodField()
@@ -118,6 +123,7 @@ class CommentDetailSerializer(ModelSerializer):
 		model = Comment
 		fields = [
 		    'id',
+		    'user',
 		    # 'content_type',
 		    # 'object_id',
 		    'content',
